8c3bdb2ccc912b681a46b5ca622104ced3584e16
badr ghanbi <bghanbi50@gmail.com>
2023-05-26 14:37:51 +0100

    add the plus icon

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,13 @@ import './App.css'
 function App() {
 
   return (
-   <div className='mx'>
-    <h2>fireGram</h2>
-    <h1>Your Pictures</h1>
+   <div className='mx-auto max-w-[1200px]'>
+    <h2 className='text-orange-300 mt-10 text-2xl'>fireGram</h2>
+    <div className='text-center'>
+      <h1 className='text-5xl mt-40'>Your Pictures</h1>
+      <p className='my-6 text-lg font-normal'> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim, earum.</p>
+    <p className='text-orange-300 text-xl border-2 border-orange-300 font-bold cursor-pointer  rounded-full  h-10 justify-center items-center inline-flex w-10 '>+</p>
+    </div>
     </div>
   )
 }
