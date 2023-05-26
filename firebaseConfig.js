734da1386c43c33891cf34d7a45ba95853b1a41e
badr ghanbi <bghanbi50@gmail.com>
2023-05-26 14:26:37 +0100

    install tailwind

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBFY1QMIankdplYdi2bqY50jU_6Rwot2iI",
+  authDomain: "photogallery-c3dd2.firebaseapp.com",
+  projectId: "photogallery-c3dd2",
+  storageBucket: "photogallery-c3dd2.appspot.com",
+  messagingSenderId: "648358178324",
+  appId: "1:648358178324:web:0c80de6cf19d914cacc101"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
