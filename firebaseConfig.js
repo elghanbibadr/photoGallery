66168b50f1460dfed7fff2e77526a0eb859b9827
badr ghanbi <bghanbi50@gmail.com>
2023-05-29 11:37:38 +0100

    clean up work environment

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getStorage } from 'firebase/storage';
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore ,serverTimestamp } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,7 +17,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const timestamp = firebase.firestore.FieldValue.serverTimestamp;
-export const projectStorage=getStorage(app)
+export const storage=getStorage(app)
+export const timestamp = serverTimestamp();
 // export const db=getFirestore(app)
 export const projectFirestore =getFirestore(app)
--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,8 @@
-
-import './App.css'
-import { db } from '../firebaseConfig'
-import { collection, getDocs, addDoc, deleteDoc, doc ,updateDoc} from "firebase/firestore";
-import { useState } from 'react'
-import { useEffect } from 'react';
-
-function App() {
-  const [myImages ,setMyImages]=useState([])
-  const collectionRef=collection(db, 'myImages')
-  const getImages = async () => {
-    const querySnapshot = await getDocs(collectionRef)
-    const imagesData = []
-    querySnapshot.forEach((doc) => {
-      imagesData.push({ id: doc.id, ...doc.data() });
-    });
-    setMyImages(imagesData)
-  }
-
-
-  useEffect(() => {
-    getImages()
-  },[])
-
-  const handleImageUpload=(e) => {
-    const file = e.target.files[0];
-    console.log(file)
-  }
-  console.log(myImages)
-
+import React from 'react'
+import { storage } from '../firebaseConfig'
+const App = () => {
   return (
-   <div className='mx-auto max-w-[1200px]'>
-    <h2 className='text-orange-300 mt-10 text-2xl'>fireGram</h2>
-    <div className='text-center'>
-      <h1 className='text-5xl mt-40'>Your Pictures</h1>
-      <p className='my-6 text-lg font-normal'> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim, earum.</p>
-    <input type="file" onChange={handleImageUpload}  className='text-orange-300 text-xl border-2 border-orange-300 font-bold cursor-pointer  rounded-full  h-10 justify-center items-center inline-flex w-10' accept="image/*"  />
-    <div className='grid grid-cols-3 gap-6  mt-10'>
-      {myImages.map(({id,imageSrc}) =>{
-         return <img className='h-full' key={id} src={imageSrc} alt="image" />
-      })}
-    </div>
-    </div>
-    </div>
+    <div>App</div>
   )
 }
 
