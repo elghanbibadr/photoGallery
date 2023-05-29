b4cb3092529833f60f6fb2604b4c2c622daed856
badr ghanbi <bghanbi50@gmail.com>
2023-05-29 12:02:33 +0100

    add the necessary logic for image upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from 'react'
 import { storage } from '../firebaseConfig'
+import { v4 } from 'uuid'
+import { ref, uploadBytes } from 'firebase/storage'
 const App = () => {
   const [imageUpload ,setImageUpload] = useState(null)
 
   const handleImageUploaded=(e) => {
     e.preventDefault();
     if(!imageUpload)return;
+    const imageRef=ref(storage, `images/${imageUpload.name + v4()}`)
+    uploadBytes(imageRef,imageUpload).then(() => {
+      alert('image uploaded')
+    });
     console.log(file)
   }
   return (
