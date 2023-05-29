f54e1ce42fe898a6e327589136e0a32a9d5c2315
badr ghanbi <bghanbi50@gmail.com>
2023-05-29 11:48:03 +0100

    add the core logic for the upoad file and install the uuid package to generate unique id's

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { storage } from '../firebaseConfig'
 const App = () => {
+  const [imageUpload ,setImageUpload] = useState(null)
+
+  const handleImageUploaded=(e) => {
+    e.preventDefault();
+    if(!imageUpload)return;
+    console.log(file)
+  }
   return (
-    <div>App</div>
+    <div>
+      <form onSubmit={handleImageUploaded} >
+      <input onChange={(e) =>setImageUpload(e.target.files[0])} type="file"   />
+    <button >upload file</button>
+      </form>
+
+    </div>
   )
 }
 
